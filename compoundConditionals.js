bc5f32e1c08000c039bfb0a87ef2586f7faa2209
BOOMS17 <judahroberts100@gmail.com>
2024-06-17 15:36:55 -0400

    Update compoundConditionals.js

--- compoundConditionals.js
+++ compoundConditionals.js
@@ -2,8 +2,8 @@
 
 // 1. Create a secret username and password.
 //  - The variables are declared already. Update the values.
-let secretUsername = ("");
-let secretPassword = ("");
+let secretUsername = ("jvr");
+let secretPassword = ("poo");
 let button = document.querySelector("button");
 
 button.addEventListener("click", function() {
@@ -11,15 +11,32 @@ button.addEventListener("click", function() {
     let password = document.querySelector(".password").value;
     let message = document.querySelector(".message");
 
+
+
     // 2. Create a compound conditional statement to check for the following:
     //  - If the username AND password is correct, update the text of the message div to say "Success!"
     
+    if (username === 'jvr' && password === 'poo')
+        document.querySelector('message').innerHTML = 'success'
+
     // CODE SOLO
     // 3. Create additional compound conditionals that check for the following:
     //  - If the username is correct AND the password is incorrect, update the text of the message div to say "Wrong password - be careful, don't mess this up too many times."
     //  - If the password is correct AND the username is incorrect, update the text of the message div to say "Wrong username - is there a typo?"
     //  - Finally, if the password is incorrect AND the username is incorrect, update the text of the message div to say "Incorrect username and password. Hmm. Suspicious."
-    
+    else if (username === 'jvr' && password !== 'poo')
+        {
+        document.querySelector('message').innerHTML = 'wrong psw'
+        }
+
+    else if (username !== 'jvr' && password === 'poo')
+    {
+    document.querySelector('message').innerHTML = 'wrong user'
+    }
+
+    else if (username === '' && password === '') {document.querySelector('message').innerHTML = 'enter something'}
+
+    else {document.querySelector('message').innerHTML = 'wrong user and wrong psw'}
     
     // Bonus: If the username OR password fields are empty, update the text of the message div to say "You need to enter SOMETHING."
     //  - Hint: What string represents an "empty" message.
